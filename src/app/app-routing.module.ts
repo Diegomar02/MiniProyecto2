42056c321df0e8497dac5846155d7b6ad5effc9c
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-05-12 08:51:42 -0600

    componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Airbnb1Component } from './airbnb1/airbnb1.component';
+import { Airbnb2Component } from './airbnb2/airbnb2.component';
 
 const routes: Routes = [
   { path:"airbnb1",component:Airbnb1Component},
+  { path:"airbnb2",component:Airbnb2Component},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,28 @@ import { Airbnb1Component } from './airbnb1/airbnb1.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { BotonSesionComponent } from './boton-sesion/boton-sesion.component';
+import { Airbnb2Component } from './airbnb2/airbnb2.component';
+import { Airbnb3Component } from './airbnb3/airbnb3.component';
+import { Airbnb4Component } from './airbnb4/airbnb4.component';
+import { Airbnb5Component } from './airbnb5/airbnb5.component';
+import { Airbnb6Component } from './airbnb6/airbnb6.component';
+import { Airbnb7Component } from './airbnb7/airbnb7.component';
+import { Airbnb8Component } from './airbnb8/airbnb8.component';
+import { Airbnb9Component } from './airbnb9/airbnb9.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BotonSesionComponent,
     Airbnb1Component,
-    BotonSesionComponent
+    Airbnb2Component,
+    Airbnb3Component,
+    Airbnb4Component,
+    Airbnb5Component,
+    Airbnb6Component,
+    Airbnb7Component,
+    Airbnb8Component,
+    Airbnb9Component,
   ],
   imports: [
     BrowserModule,
